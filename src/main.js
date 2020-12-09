ec34371d609768efe5e0413be8a604f6cd78b55e
gamalielgon <ggonzalez26@ucol.mx>
2020-12-09 12:51:00 -0600

    Added funtion sumarParesDo

--- src/main.js
+++ src/main.js
@@ -46,6 +46,18 @@ export default class App {
         }
         return impares;
     }
+    sumarParesDo() {
+        let i = 1;
+        let suma = 0;
+        do{
+            if(i%2 == 0) {
+                suma = suma + i;
+
+            }
+            i++;
+        } while (i <= 20);
+        return suma;
+    }
 }
 
 let app = new App();
@@ -53,3 +65,4 @@ console.log(app.sumarParesFor());
 console.log(app.contarImparesFor(11, 5));
 console.log(app.sumarParesWhile());
 console.log(app.contarImparesWhile(5,11));
+console.log(app.sumarParesDo());
