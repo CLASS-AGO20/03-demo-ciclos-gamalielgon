b63431290fe32b8237109c916b8339170a598725
gamalielgon <ggonzalez26@ucol.mx>
2020-12-09 12:17:18 -0600

    Added sumarParesFor

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-export default class App {}
+export default class App {
+    sumarParesFor(){
+    let suma = 0
+    for(let i = 0; i <= 20; i = i + 2){
+        suma = suma + i;
+    }
+    return suma;
+    }
+}
 
 let app = new App();
+console.log(app.sumarParesFor());
